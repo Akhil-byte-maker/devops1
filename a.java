658e1f357aaf92dbbd4f77a2a4299102606bbb99
akhil <lakideakhil123@gmail.com>
2026-09-21 14:25:34 +0530

    initial commit

---
+++ a.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 200;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
